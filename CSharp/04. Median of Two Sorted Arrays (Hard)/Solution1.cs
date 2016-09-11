d69e6fb2654f8652d0647cf00d81b14dbd3bc467
Roman-Dementiev <RDementiev@outlook.com>
2016-09-11 17:20:47 -0400

    04. Median of Two Sorted Arrays
    Added solution - 04. Median of Two Sorted Arrays

---
+++ CSharp/04. Median of Two Sorted Arrays (Hard)/Solution1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCode
+{
+	public class Solution1
+	{
+		double FindMedianSortedArray(int[] nums)
+		{
+			if (nums == null || nums.Length == 0)
+				return 0.0;
+
+			int mid = nums.Length / 2;
+			double median = nums[mid];
+			if (nums.Length % 2 == 0) {
+				median = (median + nums[mid - 1]) / 2.0;
+			}
+			return median;
+		}
+
+		public int FindKth(int k, int[] nums1, int[] nums2, out int prev)
+		{
+			Debug.Assert(k >= 0 && k < nums1.Length + nums2.Length);
+
+			prev = 0;
+			int num = 0,  i1 = 0, i2 = 0;
+			while (k >= 0)
+			{
+				prev = num;
+				if (i1 == nums1.Length) {
+					if (k > 0) prev = nums2[i2+k-1];
+					return nums2[i2+k];
+				}
+				if (i2 == nums2.Length) {
+					if (k > 0) prev = nums1[i1+k-1];
+					return nums1[i1+k];
+				}
+				if (nums1[i1] < nums2[i2]) {
+					num = nums1[i1++];
+				} else {
+					num = nums2[i2++];
+				}
+				k--;
+			}
+			return num;
+		}
+
+		public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			if (nums1 == null || nums1.Length == 0) {
+				return FindMedianSortedArray(nums2);
+			}
+			if (nums2 == null || nums2.Length == 0) {
+				return FindMedianSortedArray(nums1);
+			}
+
+			int n = nums1.Length + nums2.Length;
+			int prev, num = FindKth(n / 2, nums1, nums2, out prev);
+
+			if (n % 2 == 0) {
+				return ((double)num + (double)prev) / 2.0;
+			} else {
+				return num;
+			}
+		}
+	}
+}
---
+++ CSharp/04. Median of Two Sorted Arrays (Hard)/Solution2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using TestSample = LeetCode.TestSample<int[], int[], double>;
+
+namespace LeetCode
+{
+	public class Solution2
+	{
+		double FindMedianSortedArray(int[] nums)
+		{
+			if (nums == null || nums.Length == 0)
+				return 0.0;
+
+			int mid = nums.Length / 2;
+			double median = nums[mid];
+			if (nums.Length % 2 == 0) {
+				median = (median + nums[mid - 1]) / 2.0;
+			}
+			return median;
+		}
+
+		public int FindKth(int k,
+							int[] nums1, int start1, int count1,
+							int[] nums2, int start2, int count2)
+		{
+			Debug.Assert(start1 >= 0 && count1 >= 0 && start1 + count1 <= nums1.Length);
+			Debug.Assert(start2 >= 0 && count2 >= 0 && start2 + count2 <= nums2.Length);
+			Debug.Assert(k >= 0 && count1 + count2 > 0 && k < count1 + count2);
+
+			Debug.WriteLine("FindKth: k={0} {1} {2}", k,
+				TestSample.ToString(nums1, start1, count1),
+				TestSample.ToString(nums2, start2, count2));
+
+			if (count1 == 0) {
+				Debug.WriteLine("       => {0}", nums2[start2+k]);
+				return nums2[start2+k];
+			}
+			if (count2 == 0) {
+				Debug.WriteLine("       => {0}", nums1[start1+k]);
+				return nums1[start1+k];
+			}
+
+			int k1 = count1 / 2;
+			int k2 = count2 / 2;
+			int n1 = nums1[start1 + k1];
+			int n2 = nums2[start2 + k2];
+			Debug.WriteLine("       : k1={0} n1={1} k2={2} n2={3}", k1, n1, k2, n2);
+
+			if (k == 0) {
+				Debug.WriteLine("       => {0}", Math.Min(nums1[start1], nums2[start2]));
+				return Math.Min(nums1[start1], nums2[start2]);
+			}
+			if (k1 == 0 && k2 == 0) { // k == count1 == count2 == 1
+				Debug.WriteLine("       => {0}", Math.Max(n1, n2));
+				return Math.Max(n1, n2);
+			}
+
+			if (n1 < n2) {
+				if (k <= k1 + k2) {
+					return FindKth(k, nums1, start1, count1, nums2, start2, k2);
+				} else {
+					return FindKth(k-k1-1, nums1, start1+k1+1, count1-k1-1, nums2, start2, count2);
+				}
+			} else {
+				if (k <= k1 + k2) {
+					return FindKth(k, nums1, start1, k1, nums2, start2, count2);
+				} else {
+					return FindKth(k-k2-1, nums1, start1, count1, nums2, start2+k2+1, count2-k2-1);
+				}
+			}
+		}
+
+		public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			Debug.WriteLine("FindMedian: {0} {1}", TestSample.ToString(nums1), TestSample.ToString(nums2));
+
+			double median;
+			if (nums1 == null || nums1.Length == 0) {
+				median = FindMedianSortedArray(nums2);
+			} else if (nums2 == null || nums2.Length == 0) {
+				median = FindMedianSortedArray(nums1);
+			} else {
+				int n = nums1.Length + nums2.Length;
+				int k = n/ 2;
+				median = FindKth(k, nums1, 0, nums1.Length, nums2, 0, nums2.Length);
+				if (n % 2 == 0) {
+					median += FindKth(k-1, nums1, 0, nums1.Length, nums2, 0, nums2.Length);
+					median /= 2;
+				}
+			}
+
+			Debug.WriteLine("=> {0}\n", median);
+			return median;
+		}
+	}
+}
